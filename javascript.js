d2a536cd211162a722e5537e46c84244822f7f78
Tshego9 <149184709+Tshego9@users.noreply.github.com>
2024-09-09 18:05:25 +0200

    i created  the javascript file and wrote the logic to get the computer choice and the human choice

---
+++ javascript.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+const pick = ["Rock", "Paper", "Scisors"];
+return pick[Math.floor(math.random() * pick.length)];
+}
+console.log(getComputerChoice)
+
+function getHumanChoice() {
+    const message = prompt("Hey! you are about to play a game. Please choose between Rock, Paper and Scissor.")
+}
+console.log(getHumanChoice)
