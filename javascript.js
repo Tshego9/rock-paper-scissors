218d3fa315c06069c08be21b5839027e60114774
Tshego9 <149184709+Tshego9@users.noreply.github.com>
2024-09-10 18:26:01 +0200

    I declared the players score variables and wrote the logic to play the single round and the entire game

--- javascript.js
+++ javascript.js
@@ -5,6 +5,53 @@ return pick[Math.floor(math.random() * pick.length)];
 console.log(getComputerChoice)
 
 function getHumanChoice() {
-    const message = prompt("Hey! you are about to play a game. Please choose between Rock, Paper and Scissor.")
+    const message = prompt("Hey! you are about to play a game. Please choose between Rock, Paper and Scissors.")
 }
 console.log(getHumanChoice)
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (computerChoice === "Rock" && humanChoice === "Paper") {
+        return "You win! Paper beats Rock";
+    } else if (computerChoice === "Scissors" && humanChoice === "Paper") {
+        return "You lose! Scissors beats Paper";
+    } else if (computerChoice === "Paper" && humanChoice === "Rock") {
+        return "You lose! Paper beats Rock";
+    } else if (computerChoice === "Rock" && humanChoice === "Scissors") {
+        return "You lose! Rock beats Scissors";
+    } else if (computerChoice === "Scissors" && humanChoice === "Rock") {
+        return "You win! Rock beats Scissors";
+    } else if (computerChoice === "Paper" && humanChoice === "Scissors") {
+        return "You win! Scissors beats Paper";
+    } else {
+    } return "Draw!";
+}
+
+const humanSelection = getHumanChoice(toLoweCase(), toUpperCase());
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
+console.log(playRound)
+
+function playGame() {
+    humanScore = 0;
+    computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+
+    if (humanScore > computerScore) {
+        console.log("You won the game! Final score -You:          ${humanScore}, Computer: ${computerScore}");
+    } else if (computerScore > humanScore) {
+        console.log("you lost the game! Final score - You: ${humanScore}, Computer: ${computerScore}");
+    } else {
+        console.log("Draw! Final score - You: ${humanScore}, Computer: ${computerScore}");
+    }    
+}
+
+// Start the game
+playGame();
